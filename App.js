1554c44319041bf37bc2a1ac94c63241dd22072a
Priyanka <priyanka.dwibedi03@gmail.com>
2024-10-13 18:00:58 -0400

    Code changes from Episode 1-2

--- App.js
+++ App.js
@@ -1,31 +1,22 @@
-/**
- * <div id="parent">
- *    <div id="child1">
- *        <h1>This is H1 tag</h1>
- *        <h2>This is H2 tag</h2>
- *    </div>
- *    <div id="child2">
- *        <h1>This is H1 tag</h1>
- *        <h2>This is H2 tag</h2>
- *    </div>
- * </div>
- *
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+// const heading= React.createElement
+// ("div",{"id":"parent"},[
+//     React.createElement("div",{id:"child1"},[
+//         React.createElement("h1",{},"This is H1 tag"),
+//         React.createElement("h2",{},"This is H2 tag"),
+//     ]),
+//     React.createElement("div",{id:"child2"},[
+//         React.createElement("h1",{},"This is H1 tag from another div"),
+//         React.createElement("h2",{},"This is H2 tag from another div"),
+//     ]),
+// ]);
 
-
-
-
-const heading= React.createElement
-("div",{"id":"parent"},[
-    React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"This is H1 tag"),
-        React.createElement("h2",{},"This is H2 tag"),
-    ]),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"This is H1 tag from another div"),
-        React.createElement("h2",{},"This is H2 tag from another div"),
-    ]),
-]);
+const heading= (
+    <h1 className="head" tabIndex="5">
+        Namaste React using JSX
+    </h1>
+);
 const root= ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);

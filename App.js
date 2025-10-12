7f3d13c0bddb552037e6223bf080c3ae8e761268
Priyanka <priyanka.dwibedi03@gmail.com>
2025-10-12 15:24:48 -0400

    Example of nested divs and siblings in react

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 /**
- * 
+ * this is an example of siblings
  * <div id="parent">
  *    <div id="child">
- *       <h1>/h1>
+ *       <h1>I am h1 tag/h1>
+ *       <h2>I am h2 tag</h2>
  *     </div>
  * </div>
  * 
+ * another example below is for the nested child
+ * * <div id="parent">
+ *    <div id="child">
+ *       <h1>I am h1 tag/h1>
+ *       <h2>I am h2 tag</h2>
+ *     </div>
+ *    <div id="child2">
+ *       <h1>I am h1 tag/h1>
+ *       <h2>I am h2 tag</h2>
+ *     </div>
+ * </div>
  *  this is nested form in REACT
  */
 
@@ -13,11 +25,16 @@
 const parent = React.createElement(
 "div",
  {id:"parent"},
- React.createElement(
+ [React.createElement(
     "div",
     {id:"child"},
-    React.createElement("h1",{},"This is an example of nested div")
- )
+    [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")],
+      React.createElement(
+        "div",
+        {id:"child2"},
+        [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
+     )
+ )]
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);

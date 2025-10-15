e869a177017c0e4e221baa2777f2dde89f67a040
Priyanka <priyanka.dwibedi03@gmail.com>
2025-10-14 22:14:28 -0400

    Episode 2 of React code

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 /**
  * this is an example of siblings
  * <div id="parent">
@@ -27,12 +30,12 @@ const parent = React.createElement(
  {id:"parent"},
  [React.createElement(
     "div",
-    {id:"child"},
-    [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")],
+    {id:"child",key:"child1"},
+    [React.createElement("h1",{key:"h1-1"},"I am h1 tag"),React.createElement("h2",{key:"h2-1"},"I am h2 tag")],
       React.createElement(
         "div",
-        {id:"child2"},
-        [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
+        {id:"child2",key:"child2"},
+        [React.createElement("h1",{key:"h1-2"},"I am h1 tag"),React.createElement("h2",{key:"h2-2"},"I am h2 tag")]
      )
  )]
 );

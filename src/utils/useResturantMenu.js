2b38fa45da9fb4ea2b2f31de544f8a3828800306
priyankadwibedi <priyanka.dwibedi03@gmail.com>
2025-11-17 22:19:25 -0500

    Update useResturantMenu.js

--- src/utils/useResturantMenu.js
+++ src/utils/useResturantMenu.js
@@ -10,7 +10,7 @@ const useResturantMenu = (placeId) =>{
 
 
     const fetchData= async () => {
-        const data = await fetch(`https://corsproxy.io/?url=https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=AIzaSyBgInNLopF2_eEBsi1WX4Gv-Ie0GwRLTi4`);
+        const data = await fetch(`https://corsproxy.io/?url=https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key="YOUR_KEY"`);
         const json = await data.json();
         console.log(json);
         setResInfo(json?.result);
